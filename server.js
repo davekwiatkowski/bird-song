22f43749ac2cc930fc925573ffc58dfd6799f583
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-10-29 03:08:42 -0400

    Update heroku server file

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 const WebSocket = require('ws');
+const hasha = require('hasha');
 const server = new WebSocket.Server({port: (process.env.PORT || 5000)});
 
-// Broadcast to all.
-server.broadcast = data => {
-    server.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send('Test send!');
-        }
-    });
+// Data
+const dict = {}; // List of users and their choices
+const choice_counts = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 0,
+    'NC': 0,
 };
 
+function broadcast() {
+    server.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            // Send everyone updated data
+            client.send(
+                JSON.stringify(choice_counts)
+            );
+        }
+    });
+}
+
 server.on('connection', socket => {
-    socket.on('message', data => {
-        // Broadcast to everyone else.
-        server.clients.forEach(client => {
-            if (client !== socket && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
+    let key = null;
+
+    socket.on('message', raw_data => {
+        const data = JSON.parse(raw_data);
+
+        // Update our arrays of data
+        key = hasha(data.email);
+        if (key in dict) {
+            choice_counts[dict[key]]--;
+        }
+
+        dict[key] = data.choice;
+        choice_counts[data.choice]++;
+
+        // Broadcast to everyone
+        broadcast();
+    });
+
+    socket.on('close', () => {
+        choice_counts[dict[key]]--;
+        delete dict[key];
+        broadcast();
     });
 });
