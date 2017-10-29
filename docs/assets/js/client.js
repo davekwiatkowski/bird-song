44407a74aa92239dd45af21b4dfd111f7e920255
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-10-29 07:56:36 -0400

    Throttle client to server messages

--- docs/assets/js/client.js
+++ docs/assets/js/client.js
@@ -1,5 +1,51 @@
 let socket = null;
 
+// Returns a function, that, when invoked, will only be triggered at most once
+// during a given window of time. Normally, the throttled function will run
+// as much as it can, without ever going more than once per `wait` duration;
+// but if you'd like to disable the execution on the leading edge, pass
+// `{leading: false}`. To disable execution on the trailing edge, ditto.
+const throttle = (func, wait, options) => {
+    var timeout, context, args, result;
+    var previous = 0;
+    if (!options) options = {};
+
+    var later = () => {
+        previous = options.leading === false ? 0 : _.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+    };
+
+    var throttled = () => {
+        var now = _.now();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+
+    throttled.cancel = () => {
+        clearTimeout(timeout);
+        previous = 0;
+        timeout = context = args = null;
+    };
+
+    return throttled;
+};
+
 function clientInit() {
     // Create WebSocket connection.
     socket = new WebSocket('ws://pacific-cove-25620.herokuapp.com');
@@ -17,10 +63,10 @@ function clientInit() {
 
 function clientHandleMove(pos) {
     // Send user email and choice
-    socket.send(
+    throttle(socket.send(
         JSON.stringify({
             email: user.email,
             position: pos
         })
-    );
+    ), 250);
 }
