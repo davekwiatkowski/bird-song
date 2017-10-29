dd6998955ea77cc2303ce64b90c41425f20ceb99
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-10-29 02:53:23 -0400

    Got vote count live update working

--- docs/assets/js/client.js
+++ docs/assets/js/client.js
@@ -1,4 +1,27 @@
+let socket = null;
+
 function clientInit() {
+    // Create WebSocket connection.
+    socket = new WebSocket('ws://localhost:1337');
+
     const email = user.email;
-    console.log(email);
+
+    // Listen for messages from the server
+    socket.onmessage = event => {
+        const counts = JSON.parse(event.data);
+        Object.keys(counts).forEach(key => {
+            const val = counts[key];
+            $('#count' + key).text(val);
+        });
+    };
+}
+
+handle = data => {
+    // Send user email and choice
+    socket.send(
+        JSON.stringify({
+            email: user.email,
+            choice: data
+        })
+    );
 }
--- example/server.js
+++ example/server.js
@@ -3,20 +3,36 @@ const hasha = require('hasha');
 
 const server = new WebSocket.Server({ port: 1337 });
 
-// Data arrays
-var dict = {};
+// Data
+const dict = {}; // List of users and their choices
+const choice_counts = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 0,
+    'NC': 0,
+};
 
 server.on('connection', socket => {
-    dict[''] = null;
+    socket.on('message', raw_data => {
+        const data = JSON.parse(raw_data);
 
-    socket.on('message', data => {
         // Update our arrays of data
+        const key = hasha(data.email);
+        if (key in dict) {
+            choice_counts[dict[key]]--;
+        }
+
+        dict[key] = data.choice;
+        choice_counts[data.choice]++;
 
         // Broadcast to everyone
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 // Send everyone updated data
-                client.send(data);
+                client.send(
+                    JSON.stringify(choice_counts)
+                );
             }
         });
     });
