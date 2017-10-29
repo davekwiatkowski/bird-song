7963473b3ac0b96d9a531bd5c3390b487d73d975
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-10-29 12:41:52 -0400

    Implement rounds (time not working)

--- docs/src/js/client.js
+++ docs/src/js/client.js
@@ -1,8 +1,21 @@
 let socket = null;
+let time = 40;
+
+function countDown() {
+    let timer = setInterval(function () {
+        --time;
+        if (time < 0) {
+            time = 0;
+            clearInterval(timer);
+        }
+
+        // $("#time").text("Time: " + time + "s");
+    }, 1000);
+}
 
 function getTruePosition(pos) {
     if (nipple_pos === null) {
-        return {x: 0, y: 0};
+        return { x: 0, y: 0 };
     }
 
     return {
@@ -19,20 +32,36 @@ function clientInit() {
 
     // Listen for messages from the server
     socket.onmessage = event => {
-        const swarm_pos = JSON.parse(event.data);
-        console.log(swarm_pos);
+        const parsed_data = JSON.parse(event.data);
 
-        // For Dave:
-        // Use: swarm_pos.x and swarm_pos.y
-        // to draw the swarm's position in the UI
-
-        $(".swarm").css({
-            'transform': `translate(${swarm_pos.x}px, ${swarm_pos.y}px)`
-        });
+        // Update
+        if (parsed_data['status'] == 'update') {
+            // console.log('Received message from server: ', parsed_data['data']);
+            const swarm_pos = parsed_data['data'];
+            // console.log(swarm_pos);
+            $(".swarm").css({
+                'transform': `translate(${swarm_pos.x}px, ${swarm_pos.y}px)`
+            });
+        } else if (parsed_data['status'] == 'begin') {
+            $(".bird-tune").css("color", "#ccc");
+            $(".bird-tune").css("border-bottom", "1px solid #ccc");
+            // console.log("Beginning.");
+            // new_date = new Date();
+            // time = (new_date.getTime() - new Date(parsed_data['data']).getTime()) / 1000;
+            // countDown();
+        }
+        else { // Winner
+            let winner = parsed_data['data'];
+            winner = winner === 1 ? 4 : winner === 2 ? 3 : winner;
+            $(TUNES[winner].obj).css("color", "#5fa");
+            $(TUNES[winner].obj).css("border-bottom", "1px solid #5fa");
+        }
     };
 }
 
 function clientHandleMove(pos) {
+    // console.log('Client -> Server: ', getTruePosition(pos));
+
     // Send user email and position
     socket.send(
         JSON.stringify({
--- server.js
+++ server.js
@@ -3,29 +3,149 @@ const hasha = require('hasha');
 const server = new WebSocket.Server({port: (process.env.PORT || 5000)});
 
 // Data
-const dict = {}; // List of users and their choices
+let dict = {}; // List of users and their choices
 let size = 0;
 let swarm_pos = {
     x: 0,
     y: 0,
 };
 
+// Round info
+const max_round_time = 40; // 40 Seconds
+let round_time = 0;
+let in_round = false;
+let wait = null;
+
 function setSwarmPos(newx, newy) {
     if (newx != null && newy != null && !isNaN(newx) && !isNaN(newy) && Number.isFinite(newx) && Number.isFinite(newy)) {
         swarm_pos.x = newx;
         swarm_pos.y = newy;
     }
+    else {
+        // console.log('Could not assign: ', newx, newy);
+    }
 }
 
-function broadcast() {
+function getSwarmDeg() {
+    return Math.atan2(swarm_pos.y, swarm_pos.x) * 180 / Math.PI + 180;
+}
+
+function selectWinner() {
+    const deg = getSwarmDeg();
+
+    if (deg >= 54 && deg < 126) {
+        return 0; // 1
+    }
+    else if (deg >= 126 && deg < 198) {
+        return 4; // 5
+    }
+    else if (deg >= 198 && deg < 270) {
+        return 3; // 4
+    }
+    else if (deg >= 270 && deg < 342) {
+        return 2; // 3
+    }
+    else {
+        return 1; // 2
+    }
+}
+
+function clear() {
+    // Reset all vars
+    if (wait != null) {
+        clearInterval(wait);
+    }
+    round_time = 0;
+    in_round = false;
+
+    // console.log('Clearing to 0, 0 from: ', swarm_pos);
+}
+
+function createRound() {
+    if (in_round) {
+        return;
+    }
+
+    clear();
+
+    // console.log('Trying to make a round...');
+    round_time = new Date();
+    in_round = true;
+    swarm_pos = {
+        x: 0,
+        y: 0,
+    };
+
     try {
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                console.log(swarm_pos);
-
                 // Send everyone updated data
                 client.send(
-                    JSON.stringify(swarm_pos)
+                    JSON.stringify({
+                        'status': 'begin',
+                        'data': round_time
+                    })
+                );
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+    wait = setInterval(() => {
+        const time_now = new Date();
+
+        // Round is over
+        if (Math.floor((time_now.getTime() - round_time.getTime()) / 1000) >= max_round_time) {
+            clear();
+
+            const winner = selectWinner();
+            broadcastWinner(winner);
+
+            setTimeout(() => {
+                if (size >= 1) {
+                    createRound();
+                }
+            }, 10 * 1000);
+        }
+    }, 1000);
+}
+
+function broadcast() {
+    // console.log('Tring to broadcast. INrdoun is: ', in_round);
+    if (!in_round) {
+        return;
+    }
+
+    try {
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                // Send everyone updated data
+                client.send(
+                    JSON.stringify({
+                        'status': 'update',
+                        'data': swarm_pos
+                    })
+                );
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+function broadcastWinner(winner) {
+    try {
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                // Send everyone updated data
+                client.send(
+                    JSON.stringify({
+                        'status': 'winner',
+                        'data': winner
+                    })
                 );
             }
         });
@@ -48,6 +168,7 @@ server.on('connection', socket => {
 
             if (key in dict) {
                 const old_pos = dict[key];
+                console.log(173);
                 setSwarmPos(
                     ((swarm_pos.x * size) - old_pos.x + new_pos.x) / size,
                     ((swarm_pos.y * size) - old_pos.y + new_pos.y) / size
@@ -55,6 +176,28 @@ server.on('connection', socket => {
             }
             else {
                 size++;
+                if (in_round) {
+                    try {
+                        server.clients.forEach(client => {
+                            if (client.readyState === WebSocket.OPEN) {
+                                // Send everyone updated data
+                                client.send(
+                                    JSON.stringify({
+                                        'status': 'begin',
+                                        'data': (new Date() - round_time)
+                                    })
+                                );
+                            }
+                        });
+                    }
+                    catch (error) {
+                        console.log(error);
+                    }
+                }
+                else {
+                    createRound();
+                }
+                // console.log(181);
                 setSwarmPos(
                     ((swarm_pos.x * size) + new_pos.x) / size,
                     ((swarm_pos.y * size) + new_pos.y) / size
@@ -75,11 +218,12 @@ server.on('connection', socket => {
         try {
             size--;
             if (size == 0) {
-                setSwarmPos(0, 0);
+                clear();
             }
             else {
                 if (key in dict) {
                     const old_pos = dict[key];
+                    // console.log(206);
                     setSwarmPos(
                         ((swarm_pos.x * size) - old_pos.x) / size,
                         ((swarm_pos.y * size) - old_pos.y) / size
