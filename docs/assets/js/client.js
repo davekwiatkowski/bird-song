06ae77318eb6ba50152402fc8e0dff49e2455d68
davekwiatkowski <dave1@vt.edu>
2017-10-29 08:56:47 -0400

    swarm

--- docs/assets/js/client.js
+++ docs/assets/js/client.js
@@ -55,11 +55,13 @@ function clientInit() {
     // Listen for messages from the server
     socket.onmessage = event => {
         const swarm_pos = JSON.parse(event.data);
-        
+
         // For Dave:
         // Use: swarm_pos.x and swarm_pos.y
         // to draw the swarm's position in the UI
         $(".swarm").css({
+            top: "calc(50% - 20px)",
+            left: "calc(50% - 20px)",
             transform: `translateX(${swarm_pos.x}px) translateY(${swarm_pos.y}px)`
         });
     };
