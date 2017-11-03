67dfae22b8c8bed6e32294a437de5de2e3b1671f
davekwiatkowski <dave1@vt.edu>
2017-11-02 22:22:11 -0400

    removed unnecessary directories

--- server_old.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 5000;
-
-app.get("/", function(req,res){
-	res.send("Waddup my birdsongs");
-});
-app.listen(port);
